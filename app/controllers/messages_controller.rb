08c12036e9331231f03eca4e71d1ed0d3904401b
Maclenn77 <maclenn77@gmail.com>
2022-05-23 13:49:27 -0500

    Integration with heroku credentials

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,11 +5,10 @@ class MessagesController < ApplicationController
   def reply
     message_body = params["Body"]
     from_number = params["From"]
-    phone = ENV['TWILIO_NUMBER']
     boot_twilio
     random_word
     sms = @client.messages.create(
-      from: phone,
+      from: @phone,
       to: from_number,
       body: "Learn a new Korean word! #{@word.korean_word} (pronounced #{@word.romanja}) means #{@word.translation}"
     )
@@ -18,6 +17,7 @@ class MessagesController < ApplicationController
   private
 
   def boot_twilio
+    @phone = ENV['TWILIO_NUMBER']  
     account_sid = ENV['TWILIO_SID']
     auth_token = ENV['TWILIO_TOKEN']
     @client = Twilio::REST::Client.new account_sid, auth_token
