172645dbdef9d4fac9d86ace2d998ce2e1b6011e
Maclenn77 <maclenn77@gmail.com>
2022-05-26 01:43:20 -0500

    add tests to word

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,7 @@
 class Word < ApplicationRecord
+    validates :korean_word, presence: true, format: {
+        with: /[\p{Hangul}]/, message: 'only allows hangul'
+    }
+    validates :romanja, presence: true
+    validates :translation, presence: true
 end
--- test/models/word_test.rb
+++ test/models/word_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class WordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Invalid when korean word is not written in hangul" do
+    @invalid_word = Word.new(korean_word: 'uno', romanja: 'uno', translation: 'uno')
+     assert_equal(false, @invalid_word.valid?)
+  end
+
+  test "One error message if korean word is false" do
+    @invalid_word = Word.new(korean_word: 'uno', romanja: 'uno', translation: 'uno')
+    @invalid_word.valid?
+     assert_equal(1, @invalid_word.errors.messages.size)
+  end
+
+  test "Invalid when word doesn't have romanja" do
+    @invalid_word = Word.new(korean_word: 'uno', romanja: nil, translation: 'uno')
+     assert_equal(false, @invalid_word.valid?)
+  end
+
+  test "One error message if romanja is nil" do
+    @invalid_word = Word.new(korean_word: 'uno', romanja: 'uno', translation: 'uno')
+    @invalid_word.valid?
+     assert_equal(1, @invalid_word.errors.messages.size)
+  end
+
+  test "Invalid when word doesn't have translation" do
+    @invalid_word = Word.new(korean_word: '하나', romanja: 'nil', translation: nil)
+     assert_equal(false, @invalid_word.valid?)
+  end
+
+  test "One error message if translation is nil" do
+    @invalid_word = Word.new(korean_word: '하나', romanja: 'nil', translation: nil)
+    @invalid_word.valid?
+     assert_equal(1, @invalid_word.errors.messages.size)
+  end
 end
