dff4c9fbef18774cfb20548a01e47317a8855c80
Maclenn77 <maclenn77@gmail.com>
2022-05-27 23:14:11 -0500

    Added examples

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,14 @@
 module MessagesHelper
+    def random_number
+        Number.order('RANDOM()').first
+    end
+
+    def random_word
+        Word.order('RANDOM()').first
+      end
+
+    def help
+        "Send a message to #{@phone}. Learn a new word sending 'learn word' or learn a new number sending 'learn number'
+        Learn how to use this service writing 'help'. Visit our webpage morning-crag-46272.herokuapp.com/"
+    end
 end
