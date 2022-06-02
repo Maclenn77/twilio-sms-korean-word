b034670112588c04def37a520b30864ac54d1c52
Maclenn77 <maclenn77@gmail.com>
2022-06-01 23:21:47 -0500

    Changes to reply

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,4 @@ Rails.application.routes.draw do
       post 'reply'
     end
   end
-  # get 'messages/reply'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
